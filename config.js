fc8fb1e6eaae675f77f04f3c050f563f9307993f
webguru12 <oyetundetobi1994@gmail.com>
2023-05-12 23:58:53 +0100

    version 1

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  JWT_SECRET: "AnfFRgPrjxmGXzXC4RmgqxH03ro93n2h8ID4qgaiEoY=",
+  EMAIL: "marilyne.smith22@ethereal.email", // testing email & password
+  PASSWORD: "EVPZgG4gxymZsvWGkm",
+  ATLAS_URI:
+    "mongodb+srv://admin:admin123@cluster0.4ulc5ng.mongodb.net/?retryWrites=true&w=majority",
+};
